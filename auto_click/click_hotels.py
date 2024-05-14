cf37bd2cceb0b862784884b7ee0d2fc5f010e258
zg899 <noa.jung@gmail.com>
2024-05-14 13:45:33 +0900

    Update click_hotels.py

--- auto_click/click_hotels.py
+++ auto_click/click_hotels.py
@@ -2,12 +2,12 @@
 logPath = r'/logs'  # Linux用
 
 def hotel_url():
-    txt_file_name = "url_0514_3.txt"  # 注意路径拼接时需要添加斜杠
-    url_1 = "https://www.agoda.com/tokyo-green-palace-hotel/hotel/tokyo-jp.html?"  # URL地址
-    start_date = "2024-10-18"  # 开始日期
+    txt_file_name = "url_0514_4.txt"  # 注意路径拼接时需要添加斜杠
+    url_1 = "https://www.agoda.com/dormy-inn-maebashi-hot-spring/hotel/maebashi-jp.html?"  # URL地址
+    start_date = "2024-11-03"  # 开始日期
     num_days = 1  # 连续确认天数
     los_num = 1  # 连住天数
-    adults_num = 2  # 成人数量
+    adults_num = 1  # 成人数量
     
     hotel_urls = [txt_file_name, url_1, start_date, num_days, los_num, adults_num]
 
