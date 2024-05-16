d20d656436cc0ffd06cd0989b64f9cfe88a30121
zg899 <noa.jung@gmail.com>
2024-05-16 18:30:30 +0900

    Update click_hotels.py

--- auto_click/click_hotels.py
+++ auto_click/click_hotels.py
@@ -2,7 +2,7 @@
 logPath = r'/logs'  # Linux用
 
 def hotel_url():
-    txt_file_name = "url_0516_1.txt"  # 注意路径拼接时需要添加斜杠
+    txt_file_name = "url_0516_2.txt"  # 注意路径拼接时需要添加斜杠
     url_1 = "https://www.agoda.com/art-hotel-ishigakijima/hotel/ishigaki-jp.html?"  # URL地址
     start_date = "2024-09-03"  # 开始日期
     num_days = 1  # 连续确认天数
